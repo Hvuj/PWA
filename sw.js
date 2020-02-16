10b9b27ef123bb05d6381c97f0f6afa854d7f4c1
Eli Sigal <elis@hackerucrm.co.il>
2020-02-16 19:58:15 +0200

    pwa caching update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = "site-static";
+const staticCacheName = 'site-static';
 const assets = [
     '/',
     '/index.html',
@@ -8,18 +8,20 @@ const assets = [
     '/css/styles.css',
     '/css/materialize.min.css',
     '/img/dish.png',
-    '/https://fonts.googleapis/icon?family=Materialize+Icons',
-];
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+  ];
 
 //Install event - service worker
-self.addEventListener('install', (event) => {
-    //console.log('service worker as been installed', event);
-    caches.open(staticCacheName).then((cache)=>{
-        cache.addAll()
-    }).catch((err)=>{
-
-    })
-});
+self.addEventListener('install', evt => {
+    //console.log('service worker installed');
+    evt.waitUntil(
+      caches.open(staticCacheName).then((cache) => {
+        console.log('caching shell assets');
+        cache.addAll(assets);
+      })
+    );
+  });
 
 //Activate service worker - litsining to the activating event(activate event)
 self.addEventListener('activate', (event) => {
